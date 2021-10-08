983ebf7bc3ddf4b321245db88217c7b757e4245c
Fahad Javed <Fahad_Javed008@yahoo.com>
2021-10-08 12:47:21 +0500

    More context usage (in MainNavigation and FavoritesPage)

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
+
 import { Link } from 'react-router-dom';
+import FavoritesContext from '../../store/favorites-context';
 
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
+    const favoritesCtx = useContext(FavoritesContext);
+
     return (
         <header className={classes.header}>
             <div className={classes.logo}>React Meetups</div>
@@ -17,7 +22,12 @@ function MainNavigation() {
                     </li>
 
                     <li>
-                        <Link to="/favorites">My Favorites</Link>
+                        <Link to="/favorites">
+                            My Favorites{' '}
+                            <span className={classes.badge}>
+                                {favoritesCtx.totalFavorites}
+                            </span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
